14370ee0023c5efc040f7d57f2520d9f832a254e
k19rs090 <k19rs090@st.kyusan-u.ac.jp>
2022-01-14 17:33:15 +0900

    タイマーの変更

--- www/js/tapGame.js
+++ www/js/tapGame.js
@@ -13,7 +13,7 @@
 // mBaaSの初期化
 var ncmb = new NCMB(this.APPLICATION_KEY, this.CLIENT_KEY);
 // タイマー設定
-var countTimer = 13;
+var countTimer = 20;
 // タップ回数カウンター
 var counter = 0;
 // 「tapFlag」的のタップ可否設定
@@ -54,7 +54,7 @@ function startGame() {
   $("#list-page strong").html(String(0));
 
   // タイマーリセット
-  this.countTimer = 13;
+  this.countTimer = 23;
   // タイマーを起動
   countTime(countTimer);
 }
@@ -95,10 +95,10 @@ function saveScore(name, score) {
 // タイマー
 function countTime(time) {
   if (time > 0) {
-    if (time >= 11) {
+    if (time >= 21) {
       this.tapFlag = false;
-      $("#list-page p").html(String(time - 10));
-    } else if (time == 10) {
+      $("#list-page p").html(String(time - 20));
+    } else if (time == 20) {
       this.tapFlag = true;
       $("#list-page p").html("スタート！");
     } else if (time < 0) {
